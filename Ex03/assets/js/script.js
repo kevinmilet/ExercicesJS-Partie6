52ff9d33a15b5ac158d178c2ffea5efe963c5d50
kevinmilet <kevin.milet@gmail.com>
2020-11-09 14:29:02 +0100

    exo 3 ok

--- Ex03/assets/js/script.js
+++ Ex03/assets/js/script.js
@@ -1,1 +1,6 @@
 let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
+
+for (i = 0; i < languages.length; i++) {
+  document.write('<div>' + languages[i] + '</div>');
+}
+
