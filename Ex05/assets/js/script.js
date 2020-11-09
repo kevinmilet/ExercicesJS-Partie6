c9895205bd71f6f0c4e846dbac71ed4a066dbca4
kevinmilet <kevin.milet@gmail.com>
2020-11-09 15:52:56 +0100

    exo 5 ok

--- Ex05/assets/js/script.js
+++ Ex05/assets/js/script.js
@@ -1,1 +1,7 @@
+let firstNumber = prompt('Entrez un 1er chiffre entre 0 et 10');
+let secondNumber = prompt('Entrez un 2eme chiffre entre 0 et 10');
 
+while ((firstNumber * secondNumber) < 250) {
+  document.write('<div>' + firstNumber * secondNumber + '</div>');
+  secondNumber = firstNumber * secondNumber;
+  }
