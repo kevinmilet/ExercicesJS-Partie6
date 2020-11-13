6dc8ea59d5d3d6872103c48c468a1459ffc94403
kevinmilet <kevin.milet@gmail.com>
2020-11-13 14:26:34 +0100

    modif exo 2

--- Ex02/assets/js/script.js
+++ Ex02/assets/js/script.js
@@ -7,11 +7,8 @@ for (i = 0; i <= 100; i++) {
   // on récupére le contenu de la précédente div
   let previousContent = multi.innerHTML;
 
-  // on récupére les multiples de 15 avec le modulo de i par 15
-  let multiple = i % 15;
-  
-  // si le modulo est égale à 0 on affiche '...'
-  if (multiple == 0 && i != 0) {
+    // si le modulo est égale à 0 on affiche '...'
+  if (i % 15 == 0 && i != 0) {
     multiDiv.innerHTML = previousContent + '<div>' + '...' + '</div>';
   
   //sinon on affiche le nombre
